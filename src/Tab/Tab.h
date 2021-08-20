5ac3a03378aec6f3343dd6b5e83f41437ad1ddd7
Andrew Ushakov <redstoniska.minecraft@gmail.com>
2021-08-20 16:49:10 +0300

    feat: Tab component update according to major changes

--- src/Tab/Tab.h
+++ src/Tab/Tab.h
@@ -2,7 +2,7 @@
 #define _MATERIALIZE_LAYOUT_TAB_H_
 
 #include <Arduino.h>
-#include <vector>
+#include <list>
 #include <variant>
 #include <memory>
 
@@ -10,19 +10,10 @@
 #include "../Layout/Layout.h"
 
 template <typename T>
-class Tab : public HTMLElement
+class Tab : public HTMLElement<T>
 {
-  template <typename U>
-  friend class TabGroup;
-
 private:
   String name;
-  std::vector<std::shared_ptr<Layout<T>>> layout;
-  std::shared_ptr<DynamicComponentRegistrationService<T>> registrationService;
-
-protected:
-  virtual String getHTML();
-  virtual bool emit(size_t id, String value);
 
 public:
   /**
@@ -31,80 +22,39 @@ public:
    * @param registrationService dynamic component registration service that is used in the current page
    * @param tabName the name of the tab
    */
-  Tab(std::shared_ptr<DynamicComponentRegistrationService<T>> registrationService, String tabName);
+  Tab(DynamicComponentRegistrationService<T> *registrationService);
 
   /**
    * @brief Gets the name of the tab
    * 
    * @return String 
    */
-  String getName();
+  String getTabName();
 
-  /**
-   * @brief Creates a Layout in the tab
-   * 
-   * @return std::shared_ptr<Layout<T>> where T is type of dynamic component registration service. If the page is created with MaterializeLayout, you can use 'Layout_t' shortcut instead
-   */
-  std::shared_ptr<Layout<T>> createLayout();
+  void setTabName(String tabName);
 };
 
 // ======================= IMPLEMENTATION =======================
 
 template <typename T>
-Tab<T>::Tab(std::shared_ptr<DynamicComponentRegistrationService<T>> registrationService, String tabName)
+Tab<T>::Tab(DynamicComponentRegistrationService<T> *registrationService) : HTMLElement<T>(registrationService)
 {
-  this->registrationService = registrationService;
-
-  tabName.trim();
-  this->name = tabName;
 }
 
 template <typename T>
-String Tab<T>::getHTML()
-{
-  String html;
-
-  for (auto l : this->layout)
-  {
-    if (!l)
-      continue;
-    html += l->getHTML();
-  }
-
-  return html;
-}
-
-template <typename T>
-String Tab<T>::getName()
+String Tab<T>::getTabName()
 {
   return this->name;
 }
 
 template <typename T>
-std::shared_ptr<Layout<T>> Tab<T>::createLayout()
+void Tab<T>::setTabName(String tabName)
 {
-  auto l = std::make_shared<Layout<T>>(this->registrationService, LayoutTypes::COLUMN);
-  l->setWidth(12);
-  this->layout.push_back(l);
+  tabName.trim();
+  if (tabName.isEmpty())
+    return;
 
-  return l;
-}
-
-template <typename T>
-bool Tab<T>::emit(size_t id, String value)
-{
-  bool found = false;
-  for (auto l : this->layout)
-  {
-    if (!l)
-      continue;
-    found = l->emit(id, value);
-
-    if (found)
-      break;
-  }
-
-  return found;
+  this->name = tabName;
 }
 
 #endif //_MATERIALIZE_LAYOUT_TAB_H_
