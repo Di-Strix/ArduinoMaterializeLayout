8bd57b65bc9b6055dd66ae389782d0af20838ae8
Andrew Ushakov <redstoniska.minecraft@gmail.com>
2021-08-24 23:07:40 +0300

    refactor: split MaterializeLayout types into separate file

---
+++ src/MaterializeLayoutTypes.h
@@ -0,0 +1,16 @@
+#endif //_MATERIALIZE_LAYOUT_TYPES_H_#define _MATERIALIZE_LAYOUT_TYPES_H_
+
+#include <Arduino.h>
+#include <map>
+
+#include "ComponentFactory/ComponentFactory.h"
+
+enum class SharedStaticType
+{
+  MATERIALIZE_CSS,
+  MATERIALIZE_JS,
+  NORMALIZE_CSS,
+  APPLICATION_JS,
+};
+
+#endif //_MATERIALIZE_LAYOUT_TYPES_H_
