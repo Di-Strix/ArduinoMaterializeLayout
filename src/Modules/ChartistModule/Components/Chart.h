f823fb9e3645aae96615d8d70d60459e87cdac74
Andrew Ushakov <redstoniska.minecraft@gmail.com>
2021-08-26 19:18:37 +0300

    feat: add Chartist module which allows placing charts on a page

---
+++ src/Modules/ChartistModule/Components/Chart.h
@@ -0,0 +1,268 @@
+#ifndef _MATERIALIZE_LAYOUT_CHARTIST_H_
+#define _MATERIALIZE_LAYOUT_CHARTIST_H_
+
+#include <Arduino.h>
+#include <list>
+#include <variant>
+#include <ArduinoJson.h>
+#include <DebugPrintMacros.h>
+
+#include "../../../DynamicComponentRegistrationService\DynamicComponentRegistrationService.h"
+#include "../../../HTMLElement/HTMLElement.h"
+#include "ChartLine/ChartLine.h"
+
+enum class ChartAspectRatio
+{
+  square,        // 1:1
+  minorSecond,   // 15:16
+  majorSecond,   // 8:9
+  minorThird,    // 5:6
+  majorThird,    // 4:5
+  perfectFourth, // 3:4
+  perfectFifth,  // 2:3
+  minorSixth,    // 5:8
+  goldenSection, // 1:1.618
+  majorSixth,    // 3:5
+  minorSeventh,  // 9:16
+  majorSeventh,  // 8:15
+  octave,        // 1:2
+  majorTenth,    // 2:5
+  majorEleventh, // 3:8
+  majorTwelfth,  // 1:3
+  doubleOctave   // 1:4
+};
+
+template <typename T>
+class Chart : public HTMLElement<T>
+{
+private:
+  size_t arraySize = 24;
+  std::list<ChartLine *> lines;
+  std::list<String> legendNames;
+
+  unregisterFn unregister;
+
+  ChartAspectRatio aspectRatio = ChartAspectRatio::goldenSection;
+
+  String getAspectRatioClass(ChartAspectRatio ar);
+
+public:
+  Chart(DynamicComponentRegistrationService<T> *registrationService);
+  ~Chart();
+
+  String collectChartData();
+
+  String getHTML();
+
+  void setArraySize(size_t newSize);
+  size_t getArraySize();
+
+  void pushLegend(String legendName);
+
+  void setAspectRatio(ChartAspectRatio ar);
+  ChartAspectRatio getAspectRatio();
+
+  ChartLine *createLine();
+};
+
+// ======================= IMPLEMENTATION =======================
+
+template <typename T>
+Chart<T>::Chart(DynamicComponentRegistrationService<T> *registrationService) : HTMLElement<T>(registrationService)
+{
+  this->classList.add(F("ct-chart"));
+  this->classList.add(F("chart-overflow-visible"));
+  this->setAspectRatio(this->aspectRatio);
+
+  this->unregister = registrationService->registerDynamicGetter({this->getId(), [this]() -> UpdateMsg
+                                                                 {
+                                                                   return {F("ChartistHandler"), this->collectChartData()};
+                                                                 }});
+}
+
+template <typename T>
+Chart<T>::~Chart()
+{
+  this->unregister();
+
+  for (auto chl : this->lines)
+  {
+    chl->~ChartLine();
+    delete chl;
+  }
+}
+
+template <typename T>
+void Chart<T>::setArraySize(size_t newSize)
+{
+  this->arraySize = newSize;
+  for (auto chl : this->lines)
+  {
+    if (chl)
+      chl->setArraySize(newSize);
+  }
+}
+
+template <typename T>
+size_t Chart<T>::getArraySize()
+{
+  return this->arraySize;
+}
+
+template <typename T>
+ChartLine *Chart<T>::createLine()
+{
+  auto chl = new ChartLine(this->arraySize);
+  this->lines.push_back(chl);
+  return chl;
+}
+
+template <typename T>
+void Chart<T>::pushLegend(String legendName)
+{
+  if (this->legendNames.size() == this->arraySize)
+  {
+    this->legendNames.pop_front();
+  }
+  this->legendNames.push_back(legendName);
+}
+
+template <typename T>
+String Chart<T>::getHTML()
+{
+  String html = F("<div class=\"");
+  html += this->classList.value();
+  html += F("\" data-chart=\"true\" data-type=\"chart\" data-id=\"");
+  html += (String)this->getId();
+  html += F("\"></div>");
+  return html;
+}
+
+template <typename T>
+void Chart<T>::setAspectRatio(ChartAspectRatio ar)
+{
+  this->classList.remove(this->getAspectRatioClass(this->aspectRatio));
+  this->classList.add(this->getAspectRatioClass(ar));
+  this->aspectRatio = ar;
+}
+
+template <typename T>
+ChartAspectRatio Chart<T>::getAspectRatio()
+{
+  return this->aspectRatio;
+}
+
+template <typename T>
+String Chart<T>::getAspectRatioClass(ChartAspectRatio ar)
+{
+  switch (ar)
+  {
+  case ChartAspectRatio::square:
+    return F("square");
+
+  case ChartAspectRatio::minorSecond:
+    return F("minor-second");
+
+  case ChartAspectRatio::majorSecond:
+    return F("major-second");
+
+  case ChartAspectRatio::minorThird:
+    return F("minor-third");
+
+  case ChartAspectRatio::majorThird:
+    return F("major-third");
+
+  case ChartAspectRatio::perfectFourth:
+    return F("perfect-fourth");
+
+  case ChartAspectRatio::perfectFifth:
+    return F("perfect-fifth");
+
+  case ChartAspectRatio::minorSixth:
+    return F("minor-sixth");
+
+  case ChartAspectRatio::goldenSection:
+    return F("golden-section");
+
+  case ChartAspectRatio::majorSixth:
+    return F("major-sixth");
+
+  case ChartAspectRatio::minorSeventh:
+    return F("minor-seventh");
+
+  case ChartAspectRatio::majorSeventh:
+    return F("major-seventh");
+
+  case ChartAspectRatio::octave:
+    return F("octave");
+
+  case ChartAspectRatio::majorTenth:
+    return F("major-tenth");
+
+  case ChartAspectRatio::majorEleventh:
+    return F("major-eleventh");
+
+  case ChartAspectRatio::majorTwelfth:
+    return F("major-twelfth");
+
+  case ChartAspectRatio::doubleOctave:
+    return F("double-octave");
+
+  default:
+    return "";
+  }
+}
+template <typename T>
+String Chart<T>::collectChartData()
+{
+  size_t size = JSON_ARRAY_SIZE(this->arraySize * (this->lines.size() + 1));
+  for (auto chartLine : this->lines)
+  {
+    auto lineValues = chartLine->getValues();
+    for (auto v : lineValues)
+    {
+      size += JSON_STRING_SIZE(v.length());
+    }
+  }
+  for (auto l : this->legendNames)
+  {
+    size += JSON_STRING_SIZE(l.length());
+  }
+
+  DynamicJsonDocument doc(size);
+
+  if (doc.capacity() == 0)
+  {
+    DEBUG_ESP_PRINTF("Not enough memory to allocate DynamicJsonDocument. Required space: %i", size);
+    return "";
+  }
+
+  auto labels = doc.createNestedArray(F("labels"));
+  auto series = doc.createNestedArray(F("series"));
+
+  for (auto chartLine : this->lines)
+  {
+    auto arr = series.createNestedArray();
+
+    auto lineValues = chartLine->getValues();
+    for (auto v : lineValues)
+    {
+      arr.add(v);
+    }
+  }
+
+  for (auto l : this->legendNames)
+  {
+    labels.add(l);
+  }
+
+  doc.shrinkToFit();
+
+  String res;
+  serializeJson(doc, res);
+  // DEBUG_ESP_PRINTF("json: %s\n", res.c_str());
+
+  return res;
+}
+
+#endif //_MATERIALIZE_LAYOUT_CHARTIST_H_
