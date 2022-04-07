f2e8da3a015af2afc9709538cc3083003fa15023
Di-Strix <redstoniska.minecraft@gmail.com>
2022-04-07 09:48:58 +0200

    add FrontEnd base script

---
+++ FE.js
@@ -0,0 +1,44 @@
+class Handler {
+  onInit() {}
+  update(el, value) {}
+}
+
+class DynamicUpdateService {
+  handlers = {}
+
+  constructor() {
+    this.init = this.init.bind(this)
+    this.addHandler = this.addHandler.bind(this)
+    this.emitEvent = this.emitEvent.bind(this)
+  }
+
+  init(wsPath) {
+    Object.keys(this.handlers).forEach(key => this.handlers[key].onInit())
+    if (!wsPath) throw new Error('Websocket path must be specified')
+
+    this.ws = new WebSocket(`ws://${location.host}/${wsPath}`)
+
+    this.ws.addEventListener('message', ({ data }) => {
+      const { value, handlerId } = JSON.parse(data)
+      const el = document.querySelector(`[data-id='${key}']`)
+      const handler = this.handlers[handlerId] || {
+        update: () => console.error(`'${handlerId}' handler wasn't found`),
+      }
+
+      handler.update(el, value)
+    })
+  }
+
+  emitEvent({ target: { dataset, value } }) {
+    this.ws.send(JSON.stringify({ id: dataset.id, value }))
+  }
+
+  addHandler(handlerId, handler) {
+    if (handlerId in this.handlers) return false
+
+    this.handlers[handlerId] = handler
+    return true
+  }
+}
+
+const dynamicUpdateService = new DynamicUpdateService()
