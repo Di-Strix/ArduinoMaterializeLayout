061c0fc1af30d8a862fe3d905dbbba38e39002ae
Di-Strix <redstoniska.minecraft@gmail.com>
2022-04-25 09:59:22 +0200

    fix MaterializeLayoutTest

--- test/MaterializeLayout/TestMaterializeLayout.cpp
+++ test/MaterializeLayout/TestMaterializeLayout.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
-#include <MaterializeLayout.h>
 #include <ESPAsyncWebServer.h>
+#include <MaterializeLayout.h>
 #include <functional>
 #include <unity.h>
 #include <vector>
 
 class AsyncWebServerCountHandlers : public AsyncWebServer {
   public:
+  AsyncWebServerCountHandlers() = default;
+  virtual ~AsyncWebServerCountHandlers() = default;
+
   size_t handlersRegistered = 0;
 
   virtual AsyncWebHandler& on(String, ArRequestHandlerFunction)
@@ -35,10 +38,22 @@ class AsyncWebServerCountHandlers : public AsyncWebServer {
 };
 
 auto MLPage = MaterializeLayout("");
+auto server = new AsyncWebServer();
+
+template <class T = AsyncWebServer>
+T* setupGlobalServerInstance()
+{
+  delete server;
+  server = new T();
+
+  return static_cast<T*>(server);
+}
 
 void setUp()
 {
   MLPage = MaterializeLayout("");
+
+  setupGlobalServerInstance<AsyncWebServer>();
 }
 
 void test_get_html();
@@ -71,23 +86,23 @@ void test_get_html()
 
 void test_server_register()
 {
-  AsyncWebServerCountHandlers serverMock;
+  auto serverMock = setupGlobalServerInstance<AsyncWebServerCountHandlers>();
 
-  MLPage.registerInEspAsyncWebServer(&serverMock);
+  MLPage.registerInEspAsyncWebServer(serverMock);
 
-  TEST_ASSERT_GREATER_OR_EQUAL_MESSAGE(2, serverMock.handlersRegistered, "Should register at least 2 handlers");
+  TEST_ASSERT_GREATER_OR_EQUAL_MESSAGE(2, serverMock->handlersRegistered, "Should register at least 2 handlers");
 }
 
 void test_server_swap()
 {
   AsyncWebServerCountHandlers firstMock;
-  AsyncWebServerCountHandlers secondMock;
+  auto secondMock = setupGlobalServerInstance<AsyncWebServerCountHandlers>();
 
   MLPage.registerInEspAsyncWebServer(&firstMock);
-  MLPage.registerInEspAsyncWebServer(&secondMock);
+  MLPage.registerInEspAsyncWebServer(secondMock);
 
   TEST_ASSERT_EQUAL_MESSAGE(0, firstMock.handlersRegistered, "Should remove all handers added by ML");
-  TEST_ASSERT_GREATER_OR_EQUAL_MESSAGE(2, secondMock.handlersRegistered, "Should register at least 2 handlers");
+  TEST_ASSERT_GREATER_OR_EQUAL_MESSAGE(2, secondMock->handlersRegistered, "Should register at least 2 handlers");
 }
 
 void test_injection()
@@ -98,19 +113,18 @@ void test_injection()
   customModule.JS.file = new uint8_t(1);
   customModule.JS.fileName = "customJS";
 
-  AsyncWebServerCountHandlers serverMock;
-  MLPage.registerInEspAsyncWebServer(&serverMock);
+  auto serverMock = setupGlobalServerInstance<AsyncWebServerCountHandlers>();
+  MLPage.registerInEspAsyncWebServer(serverMock);
 
-  size_t handlersRegistered = serverMock.handlersRegistered;
+  size_t handlersRegistered = serverMock->handlersRegistered;
 
   MLPage.injectModule(customModule);
-  MLPage.registerInEspAsyncWebServer(&serverMock);
-  TEST_ASSERT_EQUAL_MESSAGE(handlersRegistered + 2, serverMock.handlersRegistered, "Should register 2 more handlers for injected module");
+  MLPage.registerInEspAsyncWebServer(serverMock);
+  TEST_ASSERT_EQUAL_MESSAGE(handlersRegistered + 2, serverMock->handlersRegistered, "Should register 2 more handlers for injected module");
 
   delete customModule.CSS.file;
   delete customModule.JS.file;
 }
-
 
 void test_injection_empty_file()
 {
@@ -119,14 +133,14 @@ void test_injection_empty_file()
   customModule.JS.file = new uint8_t(1);
   customModule.JS.fileName = "customJS";
 
-  AsyncWebServerCountHandlers serverMock;
-  MLPage.registerInEspAsyncWebServer(&serverMock);
+  auto serverMock = setupGlobalServerInstance<AsyncWebServerCountHandlers>();
+  MLPage.registerInEspAsyncWebServer(serverMock);
 
-  size_t handlersRegistered = serverMock.handlersRegistered;
+  size_t handlersRegistered = serverMock->handlersRegistered;
 
   MLPage.injectModule(customModule);
-  MLPage.registerInEspAsyncWebServer(&serverMock);
-  TEST_ASSERT_EQUAL_MESSAGE(handlersRegistered + 1, serverMock.handlersRegistered, "Should register only 1 more handler for injected module");
+  MLPage.registerInEspAsyncWebServer(serverMock);
+  TEST_ASSERT_EQUAL_MESSAGE(handlersRegistered + 1, serverMock->handlersRegistered, "Should register only 1 more handler for injected module");
 
   delete customModule.JS.file;
 }
@@ -170,12 +184,12 @@ public:
   customModule.JS.file = new uint8_t(1);
   customModule.JS.fileName = "customJS";
 
-  AsyncWebServerMock serverMock;
+  auto serverMock = setupGlobalServerInstance<AsyncWebServerMock>();
   MLPage.injectModule(customModule);
-  MLPage.registerInEspAsyncWebServer(&serverMock);
+  MLPage.registerInEspAsyncWebServer(serverMock);
 
-  TEST_ASSERT_TRUE_MESSAGE(serverMock.handlerForUrlIsRegistered("customCSS.css", false), "Should register handler with css file name");
-  TEST_ASSERT_TRUE_MESSAGE(serverMock.handlerForUrlIsRegistered("customJS.js", false), "Should register handler with js file name");
+  TEST_ASSERT_TRUE_MESSAGE(serverMock->handlerForUrlIsRegistered("customCSS.css", false), "Should register handler with css file name");
+  TEST_ASSERT_TRUE_MESSAGE(serverMock->handlerForUrlIsRegistered("customJS.js", false), "Should register handler with js file name");
 
   delete customModule.CSS.file;
   delete customModule.JS.file;
