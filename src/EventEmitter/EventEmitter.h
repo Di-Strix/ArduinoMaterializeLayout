f3d59f0287a245cd6adb2210606483076487ce1f
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-09-07 15:49:47 +0200

    Implement `EventEmitter`

---
+++ src/EventEmitter/EventEmitter.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "./Subscription.h"
+
+template <typename... Args>
+class EventEmitter {
+  protected:
+  std::list<Subscription<Args...>*> subscribers;
+
+  public:
+  EventEmitter() = default;
+
+  /**
+   * @brief Destroys all the subscribers and the Event Emitter object
+   * 
+   */
+  virtual ~EventEmitter()
+  {
+    for (auto subscriber : this->subscribers) {
+      delete subscriber;
+    }
+  };
+
+/**
+ * @brief Subscribes to an event 
+ * 
+ * @param handler function that is called when event is fired
+ * @return Subscription<Args...>* subscription control
+ */
+  Subscription<Args...>* subscribe(std::function<void(Args...)> handler)
+  {
+    auto subscription = new Subscription<Args...>(handler, [this](Subscription<Args...>* target) {
+      this->subscribers.remove(target);
+    });
+    this->subscribers.push_back(subscription);
+
+    return subscription;
+  }
+
+/**
+ * @brief Fires an event
+ * 
+ * @param args 
+ */
+  void emit(Args&&... args)
+  {
+    for (auto subscriber : this->subscribers) {
+      subscriber->emit(std::forward<Args>(args)...);
+    }
+  }
+};
---
+++ src/EventEmitter/Subscription.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <Arduino.h>
+#include <functional>
+#include <utility>
+
+template <typename... Args>
+class Subscription {
+  typedef std::function<void(Args...)> Subscriber;
+  typedef std::function<void(Subscription* target)> Unsubscriber;
+
+  protected:
+  Subscriber subscriber;
+  Unsubscriber onUnsubscribe;
+
+  public:
+  /**
+   * @brief Construct a new Subscription object
+   *
+   * @param subscriber subscriber function
+   * @param onUnsubscribe Called when subscriber wants to unsubscribe
+   */
+  Subscription(Subscriber subscriber, Unsubscriber onUnsubscribe)
+  {
+    this->subscriber = subscriber;
+    this->onUnsubscribe = onUnsubscribe;
+  };
+
+  /**
+   * @brief Destroy the Subscription object and calls unsubscribe handler
+   *
+   */
+  virtual ~Subscription()
+  {
+    this->onUnsubscribe(this);
+  };
+
+/**
+ * @brief Calls subscriber with provided args
+ * 
+ * @param args 
+ */
+  void emit(Args&&... args)
+  {
+    this->subscriber(std::forward<Args>(args)...);
+  }
+
+/**
+ * @brief Unsubscribes and deletes subscription object
+ * 
+ */
+  void unsubscribe()
+  {
+    delete this;
+  }
+};
